b9985ba2627e6d64b1489181163dd809927e2e8f
mrmnmly <mrmnmly@gmail.com>
2019-10-13 12:05:32 +0200

    Configured bundler, added basic files.

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,48 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js'
+  },
+  devServer: {
+    port: 8080,
+    inline: true,
+    disableHostCheck: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
+      },
+      {
+        test: /\.(js)$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?ass$/i,
+        use: ['style-loader', 'css-loader?sourceMap=true&url=false', 'sass-loader']
+      }
+    ]
+  }
+});
